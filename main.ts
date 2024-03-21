1da4aa7df5132494a7a36eacb35bad70cbba5d90
Mirmuhammadbhanbhro <151605191+Mirmuhammadbhanbhro@users.noreply.github.com>
2024-03-21 01:12:20 -0700

    calculator
    This is a simple calculator

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import chalk from "chalk";
+const number = await inquirer.prompt([
+    {message:"enter the first number", type: "number", name:"firstNumber"},
+    {message:"enter the second number", type: "number", name:"secondNumber"},
+    
+    {
+         message:"enter the operator you want",
+         type:"list",
+         name:"operator",
+         choices:["Addition","subtraction","Multiplication","Division"],
+    },
+]);
+ if(number.operator === "Addition"){
+    console.log(number.firstNumber + number.secondNumber);
+ }else if(number.operator === "subtraction"){
+    console.log(number.firstNumber - number.secondNumber);
+ }else if(number.operator === "Multiplication"){
+    console.log(chalk.blue(number.firstNumber * number.secondNumber));
+ }else if(number.operator === "Division"){
+    console.log(number.firstNumber / number.secondNumber);
+ }else{
+    console.log("enter the valid operator");
+ }
